a3405f5c3152ffdd06908ed88a898991ee7319a7
Moulya-KM <82828337+Moulya-KM@users.noreply.github.com>
2022-03-21 12:15:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model=joblib.load('spam-ham(1)')
+model=joblib.load('spam-ham')
 st.title('SPAM HAM CLASSSIFIER')
 ip=st.text_input('enter the message')#It will tske the message from the console
 op=model.predict([ip])
