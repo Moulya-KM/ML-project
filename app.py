975985c6d5eea2e875a2222651cca4ea930ffe06
Moulya-KM <82828337+Moulya-KM@users.noreply.github.com>
2022-03-21 12:12:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import joblib
 model=joblib.load('spam-ham(1)')
-st.title('SPAM-HAM CLASSIFIER')
-ip=st.text_input('Enter your message')
+st.title('SPAM HAM CLASSSIFIER')
+ip=st.text_input('enter the message')#It will tske the message from the console
 op=model.predict([ip])
-if st.button('Predict'):
+if st.button('Predict'):  #here it is clicking of the button
   st.title(op[0]) 
           
